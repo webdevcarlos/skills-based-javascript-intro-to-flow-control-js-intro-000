3c8069c348d5ef71042b6ec88df5662c2c7a7350
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-06 15:05:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,29 +1,29 @@
 function basicTeenager(age) {
   if (age>=13 && age<=19) {
-    return'You are a teenager!';
+    return"You are a teenager!";
   }
 }
 
 function teenager(age) {
   if (age>=13 && age<=19){
-    return 'You are a teenager!';
+    return "You are a teenager!";
   } else {
-    return 'You are not a teenager';
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if (age>=13 && age<=19){
-    return 'You are a teenager!';
+    return "You are a teenager!";
   } else if (age<=12) {
-    return 'You are a kid';
+    return "You are a kid";
   } else {
-    return 'You are a grownup';
+    return "You are a grownup";
   }
 }
 
 function ternaryTeenager(age) {
-  age>=13 && age<=19 ? return 'You are a teenager': return 'You are not a teenager';
+  age>=13 && age<=19 ? return "You are a teenager" : return "You are not a teenager";
 }
 
 function switchAge(age) {
